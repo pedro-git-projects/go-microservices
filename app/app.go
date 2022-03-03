85602269e66a76346f5a62b525608d724040a547
pedro-git-projects <pedro.generic.email@gmail.com>
2022-03-02 23:14:23 -0300

    gorilla mux

--- app/app.go
+++ app/app.go
@@ -1,23 +1,38 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var port string = ":8080"
 
 func Start() {
 
-	mux := http.NewServeMux()
+	router := mux.NewRouter()
 
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/costumers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
 
 	log.Print("starting application on port" + port)
-	err := http.ListenAndServe(port, mux)
+	err := http.ListenAndServe(port, router)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, vars["customer_id"])
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post request recieved")
 }
