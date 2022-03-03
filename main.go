6f8b5cf306fa2357dfcecf0965364aef263c40c1
pedro-git-projects <pedro.generic.email@gmail.com>
2022-03-02 22:45:52 -0300

    encoding json|xml

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+)
+
+type Customer struct {
+	Name    string `json:"name" xml:"name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zip_code"`
+}
+
+func main() {
+
+	// defining routes
+	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/costumers", getAllCustomers)
+
+	// starting server
+	http.ListenAndServe("localhost:8080", nil)
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Ashish", "New Delhi", "110075"},
+		{"José", "São Paulo", "180075"},
+	}
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
+}
